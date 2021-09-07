7b4e25f7c7a9dfc848dd767c5f0a101aa55a4f09
Agata Szulc <agataszulc97@gmail.com>
2021-09-07 15:02:49 +0200

    fixed database

--- myjs.js
+++ myjs.js
@@ -1,6 +1,6 @@
 //API-key 61374ddb43cedb6d1f97edc6
 
-const url = "https://serialkillers-7bdb.restdb.io/rest/serial-killers-database";
+const url = "https://serialkillers-7bdb.restdb.io/rest/new-db";
 
 const options = {
   headers: {
