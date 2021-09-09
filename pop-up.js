78377f2405e5e6c4f2c4faa7069a2f7f42479d85
ailin <aili0057@stud.kea.dk>
2021-09-09 10:35:42 +0200

    pop-up page is working now(fixed js)

--- pop-up.js
+++ pop-up.js
@@ -1,7 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
-const url =
-  "https://serialkillers-7bdb.restdb.io/rest/serialkillers/(6137607f7b91b61a00025a3c)";
+const url = "https://serialkillers-7bdb.restdb.io/rest/new-db/" + id;
 
 const options = {
   headers: {
@@ -11,16 +10,19 @@ const options = {
 
 fetch(url, options)
   .then((response) => response.json())
-  .then((data) => showSk(data));
+  .then((data) => {
+    showSk(data);
+    console.log(data);
+  });
 
 function showSk(sk) {
   //   document.querySelector(".skc-pic").src = ;
   document.querySelector(".skc-name").textContent = sk.alias;
-  document.querySelector(".skc-real-name").textContent = sk.realname;
+  document.querySelector(".sk-real-name").textContent = sk.realname;
   document.querySelector(".location").textContent = sk.location;
-  document.querySelector(".years").textContent = sk.years;
+  document.querySelector(".years").textContent = sk.activity;
   document.querySelector(".killstreak").textContent = sk.killstreak;
-  document.querySelector(".gender").textContent = sk.gender;
+  //   document.querySelector(".gender").textContent = sk.gender;
   document.querySelector(".motive").textContent = sk.motive;
   document.querySelector(".weapon").textContent = sk.weapon;
 }
